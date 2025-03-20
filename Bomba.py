ed3447812063546886ad74a60ef2c7b45ec55abc
Adrian Macauley <s225733@dtu.dk>
2025-03-20 16:38:41 +0100

    Funky algorithm added.

--- Bomba.py
+++ Bomba.py
@@ -1,10 +1,47 @@
-import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
-G = nx.Graph()
-V = (1,2)
-G.add_edges_from([V,(2,3),(1,3),(2,4)])
-labelmap = dict(zip(G.nodes(), ["A", "B", "C", "D"]))
-nx.draw(G, labels=labelmap, with_labels=True)
-plt.show()
+
+class GraphWithCoordinates:
+    def __init__(self):
+        self.graph = nx.Graph()
+        self.positions = {}
+
+    def add_node(self, name, x, y):
+        self.graph.add_node(name, pos=(x, y))
+        self.positions[name] = (x, y)
+
+    def add_edge(self, node1, node2):
+        #if node1 in self.graph and node2 in self.graph:
+       self.graph.add_edge(node1, node2)
+        #else:
+         #   raise ValueError("Both nodes must exist in the graph.")
+
+    def draw_graph(self):
+        plt.figure(figsize=(60, 60))
+        nx.draw(self.graph, self.positions, with_labels=True, node_color='lightblue', edge_color='gray', node_size=700)
+        plt.show()
+
+
+# Example usage
+graph = GraphWithCoordinates()
+
+import random
+
+# Generate 30 nodes with random coordinates
+defined_nodes = {chr(65 + i): (random.randint(0, 10), random.randint(0, 10)) for i in range(800)}
+
+# Add nodes using a loop
+for name, (x, y) in defined_nodes.items():
+    graph.add_node(name, x, y)
+
+# Generate random edges between nodes
+edges = [(random.choice(list(defined_nodes.keys())), random.choice(list(defined_nodes.keys()))) for _ in range(40)]
+
+# Add edges using a loop
+for node1, node2 in edges:
+    if node1 != node2:  # Avoid self-loops
+        graph.add_edge(node1, node2)
+
+
+graph.draw_graph()
