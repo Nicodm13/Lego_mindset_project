baa1a89f7fdc41e174afc1326937bafb7953eed2
sjbrandt <144696315+sjbrandt@users.noreply.github.com>
2025-03-20 14:12:21 +0100

    implement motor functionality to controller.py

--- robot/controller.py
+++ robot/controller.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, InfraredSensor, GyroSensor
+from pybricks.parameters import Port, Button, Stop
+
 from typing import List
 from pathfinding.node import Node
 from pathfinding.direction import Direction
@@ -8,6 +13,24 @@ class Controller:
     
     def __init__(self, grid: Grid):
         self.grid = grid
+        
+        # Initialize EV3 Brick
+        self.ev3 = EV3Brick()
+
+        # Initialize motors
+        self.left_motor = Motor(Port.A)
+        self.right_motor = Motor(Port.B)
+        self.spinner_motor = Motor(Port.C)
+
+        # Initialize Infrared Sensor on Port.S1
+        self.ir_sensor = InfraredSensor(Port.S1)
+
+        # Initialize Gyro Sensor on Port.S2
+        self.gyro_sensor = GyroSensor(Port.S2)
+        self.gyro_sensor.reset_angle(0)
+
+        # Display message
+        self.ev3.screen.print("IR & Gyro Ready")
     
     def set_target(target: Node):
         return
@@ -25,17 +48,51 @@ class Controller:
         xdiff = start.x - target.x
         ydiff = start.y - target.y
         
-        direction = Direction.from_offset(xdiff, ydiff)
-        self.rotate_to(direction)
+        angle = offset_to_angle(xdiff, ydiff)
+        self.rotate_to(angle)
         
         distance = self.grid.get_distance(start, target)
         self.drive(distance)
+        
+        def offset_to_angle(xdiff, ydiff):
+            angle = 180
+            angle = angle + xdiff * 90
+            angle = angle + ydiff * 45
+
+            return angle
     
     def get_direction():
         return
     
-    def drive(distance: float):
-        return
+    def drive(self, distance: float):
+        angle = 360 * distance
+        self.left_motor.run_angle(speed=60, rotation_angle=angle)
+        self.right_motor.run_angle(speed=60, rotation_angle=angle)
     
-    def rotate_to(offset: Direction):
-        return
+    # Rotate Method (positive = right turn, negative = left turn)
+    def rotate_to(self, target_angle, speed=150):
+        self.ev3.screen.clear()
+        self.ev3.screen.print("Rotating {}°".format(target_angle))
+
+        # Reset gyro
+        self.gyro_sensor.reset_angle(0)
+        
+        # Determine direction
+        if target_angle > 0:
+            # Right turn
+            self.motor_left.run(speed)
+            self.motor_right.run(-speed)
+        else:
+            # Left turn
+            self.motor_left.run(-speed)
+            self.motor_right.run(speed)
+
+        # Keep turning until desired angle reached
+        while abs(self.gyro_sensor.angle()) < abs(target_angle):
+            pass  # Just wait
+        
+        # Stop motors
+        self.motor_left.stop(Stop.BRAKE)
+        self.motor_right.stop(Stop.BRAKE)
+        
+        self.ev3.screen.print("Done!")
