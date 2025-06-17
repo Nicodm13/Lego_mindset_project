022f1b91556ee7a2d93f4bf813f1dd59d5bcc332
Freb <frederik.bode@hendrichsen.d>
2025-06-17 19:06:58 +0200

    cap set to 720 in find_robot.py

--- util/find_robot.py
+++ util/find_robot.py
@@ -232,6 +232,8 @@ def debug_robot_detection():
     
     # Initialize the camera
     cap = cv2.VideoCapture(1)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
     if not cap.isOpened():
         print("Error: Could not open camera.")
         # Return default values if camera fails
